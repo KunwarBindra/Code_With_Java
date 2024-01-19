0051512c6e4e35f84d94e4751af3ccb743364c2f
kunwarjeetbindra-nexg <kunwarjeetbindra@viewlift.com>
2024-01-19 22:34:09 +0530

    rotated array

--- StringsSorting.java
+++ StringsSorting.java
@@ -11,16 +11,31 @@ public class StringsSorting {
 
     public static void main(String []args) {
         String arr[] = {"Kunwar", "Raunak", "Karan", "Samar", "Arushi", "Pawandeep", "Vikram", "Arjun", "Amardeep", "Daman", "Sahiba", "Chinki"};
-        for (int i=0; i<arr.length-1; i++) {
-            int smallest = i;
-            for (int j=i+1; j<arr.length; j++) {
-                if (arr[smallest].compareTo(arr[j]) > 0) {
-                    smallest = j;
-                }
+
+        // Using Selection sort
+
+        // for (int i=0; i<arr.length-1; i++) {
+        //     int smallest = i;
+        //     for (int j=i+1; j<arr.length; j++) {
+        //         if (arr[smallest].compareTo(arr[j]) > 0) {
+        //             smallest = j;
+        //         }
+        //     }
+        //     String temp = arr[smallest];
+        //     arr[smallest] = arr[i];
+        //     arr[i] = temp;
+        // }
+
+        // Using Insertion sort
+
+        for (int i=1; i<arr.length; i++) {
+            String current = arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j].compareTo(current) > 0) {
+                arr[j+1] = arr[j];
+                j--;
             }
-            String temp = arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i] = temp;
+            arr[j+1] = current;
         }
         printArr(arr);
     }
